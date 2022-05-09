0161c060d292ea89493fda7cabe6eb22ec2573c2
rafaelspa <rsdepaula67@gmail.com>
2022-05-09 17:26:30 -0300

    Resumo primeira aula

--- MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
+++ MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
@@ -1,7 +1,17 @@
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
-        // A seguinte linha é para imprimir um calculo de 2+2
         System.out.println(2+2);
+        System.out.println(2+6);
+        System.out.println("ola texto");
+        System.out.println(10/3);
+
+        // comentarios // -> sao uteis para indicar comentarios/notas nos projetos
+        // ponto de entrada - entry-point -> é o ponto onde todos os sistemas procuram no seu projeto para
+        // começar a iniciar o projeto
+        // metodos: sao instrucoes que define para o programa executar . Exemplo:
+        // instrucao chamada System.out.println("")
+        // propriedades/variaveis -> será onde iremos armazenar dados para manipula-los no futuro
+
     }
 }
