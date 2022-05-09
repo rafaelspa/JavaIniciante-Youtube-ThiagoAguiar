ee4d37a34ea22c8dffed690385da5c46bfcd43a2
rafaelspa <rsdepaula67@gmail.com>
2022-05-09 17:16:12 -0300

    Exemplo de nova funcao definida

--- MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
+++ MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
@@ -1,6 +1,10 @@
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
+        minhaInstrucaoDeImprimir();
+    }
+
+    static void minhaInstrucaoDeImprimir() {
         System.out.println("Olá mundo, eu sou o primeiro programa do Tiago");
     }
 }
