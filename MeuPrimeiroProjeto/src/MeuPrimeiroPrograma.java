43ba06c7b84a02af45f6fe9b9729f2b5f1e7752e
rafaelspa <rsdepaula67@gmail.com>
2022-05-10 14:51:42 -0300

    Aula: Erros na Programacao

--- MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
+++ MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
@@ -1,29 +1,14 @@
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
-        // variaveis -> tipo: Textos (Strings)
-        // variaveis -> tipo: Numeros (Int)
-        // variaveis -> tipo: Numeros fracionados (Double)
-        // variaveis -> tipo: booleanas (verdadeiro | falso)
+        // 2 TIPOS DE ERROS
+        // ERRO DE COMPILAÇÃO
+        String text = "Olá mundo";
+        System.out.println(text);
 
-        // TIPOS DE DADOS
-        String login = "Olá"; // Representacao de Texto (ASCII)
-        int number1 = 10; // Representacao de numeros inteiros
-        double percent = 24.30; // Representacao de numeros fracionados
-        boolean isDriver = true; // Representacao booleana (estado verdadeiro, falso)
-
-
-
-        System.out.println(login);
-        System.out.println(number1);
-        System.out.println(percent);
-        System.out.println(isDriver);
-
-        // variaveis
-        // SAO ESPACOS DE MEMORIA ALOCADO (REPRESENTADO POR TIPOS)
-
-        int number; //DECLARANDO VARIAVEL
-        number = 55;
-        System.out.println(number);
+        // ERRO DE EXECUÇÃO (Exception)
+        int x = 10;
+        int y = 0;
+        System.out.println(x/y);
     }
 }
