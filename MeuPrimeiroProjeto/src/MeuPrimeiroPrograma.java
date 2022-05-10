aa06a18c8aecdc94f2d9f93471c2af110df4196f
rafaelspa <rsdepaula67@gmail.com>
2022-05-10 15:20:04 -0300

    Aula: Argumentos

--- MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
+++ MeuPrimeiroProjeto/src/MeuPrimeiroPrograma.java
@@ -1,20 +1,8 @@
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
-        calc(10, 5); // argumentos
-    }
-
-    static void calc(int x, int y) {
-        // static void > declaracao padrao (falaremos depois sobre isso, mas precisa!)
-        // sum > NOME DA FUNCAO, GERALMENTE USAMOS VERBOS PARA DEFINIR O QUE A FUNÇÃO FAZ
-        // () > PARENTESES USAMOS PARA DEFINIR O QUE CHEGARÁ NAQUELA FUNÇÃO (parametros)
-        // (tipo nomeDaVariavel)
-        // {} > bloco de código que será executado
-
-
-        System.out.println(x + y);
-        System.out.println(x - y);
-        System.out.println(x / y);
-        System.out.println(x * y);
+        System.out.println("Olá, " + args[0]);
+        System.out.println("Olá, " + args[1]);
+        System.out.println("Olá, " + args[2]);
     }
 }
