7ca00438c76f5ac3ad9ad58045466962f46b4d6d
rafaelspa <rsdepaula67@gmail.com>
2022-05-11 13:45:33 -0300

    Aula: Coleção. Lista. ArrayList

--- MeuPrimeiroProjeto/src/co/tiagoaguiar/App.java
+++ MeuPrimeiroProjeto/src/co/tiagoaguiar/App.java
@@ -1,18 +1,28 @@
 package co.tiagoaguiar;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
-        User[] users = new User[]{
-                new User("Tiago", "Aguiar"),
-                new User(),
-                new User("Flávia", "Silva")
-        };
-        // |x|x|x|x|
+        List<User> users = new ArrayList<>();
 
-        System.out.println(users[2].getFirstName());
-        System.out.println(users[2].getLastName());
-}
+        int i = 0;
+        while (i < 10) {
+            User actual = new User("Nome " + i, "Sobrenome " + i);
+            users.add(actual);
+            i++;
+        }
+
+        System.out.println(users.get(9).getFirstName());
+        System.out.println(users.get(9).getLastName());
+
+        User user11 = new User("User 11", "Sobrenome 11");
+        users.add(user11);
+
+
+        System.out.println(users.get(10).getFirstName());
+        System.out.println(users.get(10).getLastName());
+    }
 }
