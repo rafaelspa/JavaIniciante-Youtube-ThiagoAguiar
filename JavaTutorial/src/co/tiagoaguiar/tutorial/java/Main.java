d0978d016b134bbfb1d6d3d3dedae88882882312
rafaelspa <rsdepaula67@gmail.com>
2022-05-11 14:24:14 -0300

    Métodos do ArrayList

--- JavaTutorial/src/co/tiagoaguiar/tutorial/java/Main.java
+++ JavaTutorial/src/co/tiagoaguiar/tutorial/java/Main.java
@@ -1,15 +1,42 @@
 package co.tiagoaguiar.tutorial.java;
 
-
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] names = new String[10];
-        System.out.println(Arrays.toString(names));
-        Arrays.fill(names, "Desconhecido");
-        
-        System.out.println(Arrays.toString(names));
+        // String[] => Fixo
+        // new ArrayList<String>() => Dinâmicos
+
+        List<String> names = new ArrayList<>();
+        // como adicionar elementos no array?
+
+        names.add("Nome 1");
+        names.add("Nome 2");
+        names.add("Nome 3");
+        names.add("Nome 4");
+
+        // como buscar um elemento específico pelo índice get()
+        for(int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+
+        // buscar o índice a partir de um elemento
+        int index = names.indexOf("Nome 2");
+        System.out.println(index);
+
+        // verifica se a lista esta vazia
+        System.out.println(names.isEmpty());
+
+        // verifica se a lista contém algum elemento
+        System.out.println(names.contains("Nome 14"));
+
+        // verificar o tamanho da lista
+        System.out.println(names.size());
+
+        // limpar de vez a lista (remover todos os elementos)
+        names.clear();
+        System.out.println(names.isEmpty());
     }
 }
