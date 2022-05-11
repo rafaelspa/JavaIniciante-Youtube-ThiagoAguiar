6d1b3b2bd657df20d72bbdeef4d7932720651260
rafaelspa <rsdepaula67@gmail.com>
2022-05-11 13:28:43 -0300

    Aula: Arrays-OOP

--- MeuPrimeiroProjeto/src/co/tiagoaguiar/App.java
+++ MeuPrimeiroProjeto/src/co/tiagoaguiar/App.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        User userA = new User();
-        userA.setFirstName("Tiago");
+        User[] users = new User[10];
+        // |x|x|x|x|
 
+        for(int i = 0; i < users.length; i++){
+            User actual = new User();
+            actual.setFirstName("Nome " + i);
+            actual.setLastName("Sobrenome " + i);
+            users[i] = actual;
+        }
 
-        User userB = new User();
-        userB.setFirstName("Apple Incorporation");
-        System.out.println(userB.getFirstName());
-    }
+        System.out.println(users[2].getFirstName());
+        System.out.println(users[2].getLastName());
+}
 }
