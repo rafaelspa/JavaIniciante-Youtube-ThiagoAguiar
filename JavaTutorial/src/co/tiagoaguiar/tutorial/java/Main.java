ac8793a5ef2a7e7109f9873e798930c4e6b864ac
rafaelspa <rsdepaula67@gmail.com>
2022-05-11 14:11:00 -0300

    Aula: Preenchendo Arrays

--- JavaTutorial/src/co/tiagoaguiar/tutorial/java/Main.java
+++ JavaTutorial/src/co/tiagoaguiar/tutorial/java/Main.java
@@ -6,20 +6,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        // 3. comparando arrays
-        int[] numbersA = new int[]{1, 2, 3};
-        int[] numbersB = new int[]{1, 2, 3};
-        int[] numbersC = numbersA;
-
-        System.out.println(numbersA == numbersB);
-        System.out.println(numbersA.equals(numbersB));
-        System.out.println(Arrays.equals(numbersA, numbersB));
-        System.out.println(numbersA + " " + numbersB);
-
-        System.out.println(numbersA == numbersC);
-        System.out.println(numbersA.equals(numbersC));
-        System.out.println(Arrays.equals(numbersA, numbersC));
-        System.out.println(numbersA + " " + numbersC);
-
+        String[] names = new String[10];
+        System.out.println(Arrays.toString(names));
+        Arrays.fill(names, "Desconhecido");
+        
+        System.out.println(Arrays.toString(names));
     }
 }
