d9de8cb51152ec02c2fe06a5fd767776deb6978f
rafaelspa <rsdepaula67@gmail.com>
2022-05-11 14:38:12 -0300

    Aula: Sobrecarga de Métodos

--- JavaTutorial/src/co/tiagoaguiar/tutorial/java/Main.java
+++ JavaTutorial/src/co/tiagoaguiar/tutorial/java/Main.java
@@ -8,21 +8,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        // String[] => Fixo
-        // new ArrayList<String>() => Dinâmicos
-
-        List<String> names = new ArrayList<>();
-        // como adicionar elementos no array?
-
-        names.add("Nome 1");
-        names.add("Nome 2");
-        names.add("Nome 3");
-        names.add("Nome 4");
-
-        Collections.sort(names);
-
-        for(String name : names) {
-            System.out.println(name);
-        }
+        User userA = new User("Tiago", "Aguiar");
+        System.out.println(userA.output());
+        System.out.println(userA.output(false));
     }
 }
---
+++ JavaTutorial/src/co/tiagoaguiar/tutorial/java/User.java
@@ -0,0 +1,41 @@
+package co.tiagoaguiar.tutorial.java;
+
+import java.util.Locale;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String output() {
+        return firstName.toUpperCase() + " " + lastName.toUpperCase();
+    }
+
+    public String output(boolean showLastName) {
+        if (showLastName) {
+            return output();
+        }
+        return firstName;
+    }
+}
